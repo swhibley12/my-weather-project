e83ceae10a6e2bff587fe9a92a04e610a9f388f6
sarah <swhibley12@gmail.com>
2021-08-24 12:19:27 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -23,8 +23,6 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
-celsiusTemperature = response.data.main.temp;
-
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -84,17 +82,5 @@ searchForm.addEventListener("submit", handleSubmit);
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  let temperatureElement = document.querySelector("#Temperature");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-let celsiusTemperature = null;
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 searchCity("Vancouver");
